c197c40929191f5f4beabfbe1848c1df80e9af20
Craig Iturbe <citurbe@gmail.com>
2016-11-12 13:26:38 -0500

    restaurant search box appears when route is selected

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 $(function(){
   $('#origin').css('visibility','hidden')
   $('#destination').css('visibility','hidden')
+  $('#chooser').css('visibility', 'hidden')
 
   populateLines()
 
@@ -16,9 +17,22 @@ $(function(){
   // reveal destination selector
   $('#originSelect').change( () => {
     let value = $('#originSelect').val()
+    alert(value)
     $('#destination').css('visibility','visible')
     event.preventDefault()
     populateDestinationStops(value)
+  })
+
+  //reveal search box
+  $('#destinationSelect').change( () => {
+    event.preventDefault()
+    $('#chooser').css('visibility', 'visible')
+  })
+
+  //capture search data
+  $('#chooserButton').click( () => {
+    let searchTerm = $('#foodChoice').val()
+    alert(searchTerm)
   })
 
 })
