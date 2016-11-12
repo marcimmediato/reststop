1133a5b9e9f06515230b6908b4ba77be741c1f85
wcpines <colby@aptible.com>
2016-11-12 14:29:43 -0500

    wat

--- yelpFuckery.js
+++ yelpFuckery.js
@@ -1,15 +1,15 @@
-oauth_consumer_key
-oauth_token	
-oauth_signature_method	
-oauth_signature	
-oauth_timestamp
-oauth_nonce	
+// oauth_consumer_key
+// oauth_token	
+// oauth_signature_method	
+// oauth_signature	
+// oauth_timestamp
+// oauth_nonce	
 
-var auth = {
-  consumerKey: process.env.CONSUMER_KEY
-  consumerSecret : process.env.CONSUMER_SECRET,
-  accessToken : process.env.TOKEN,
-  accessTokenSecret : process.env.TOKEN_SECRET,
+var yelpAuth = {
+  consumerKey : 'LIw94oEOKXq9a43hMCIMRQ',
+  consumerSecret : 'AC83a7_CgbwxL31wrZR_MroHvvs',
+  accessToken : '3-2XYA_-k2J2Pjw-LhUUYKoWHf9eFWsq',
+  accessTokenSecret : 'ylR5GLN4-F-Gl8PsZ1Ap2ET9Q2k',
   serviceProvider : {
     signatureMethod : "HMAC-SHA1"
   }
