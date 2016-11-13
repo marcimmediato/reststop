da0fd8843dea8f857406d7fc523b9ebd18387d4b
wcpines <colby@aptible.com>
2016-11-13 13:56:42 -0500

    Seeds, auth, yelp v3

--- app/adapters/yelpAPI.js
+++ app/adapters/yelpAPI.js
@@ -1,45 +1,25 @@
 // Make the ajax request
-    // yelpUrl = 'https://api.yelp.com/v2/search?term=food&location=San+Francisco',
-// When the ajax request is completed, find the img,
-  // someData.artists.items[0].images[0].url
+// yelpUrl = 'https://api.yelp.com/v2/search?term=food&location=San+Francisco',
 
 function restaurantsAdapter(searchTerm){
-   return $.ajax({
-  method: "GET",
-  url: `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist`
-  // data: {q: artist_name, type: 'artist'}
+  return $.ajax({
+    method: "GET",
+    url: `${yelpURL}?term=${searchTerm}`
   }).done(function( response ) {
 
-    let firstArtist = response.artists.items[0]
-    new Artist(firstArtist.name, firstArtist.popularity, firstArtist.images[0].url)
   })
-
 }
+
 $(function(){
   $('form').on('submit', function(event){
     event.preventDefault()
-    let searchTerm = $('#artist_name').val()
-    artistsAdapter(searchTerm).done(function(){
-      store.artists.forEach(function(artist){
-        $('.images').append(`<img src='${artist.imageUrl}' />`)
-      })
+    let searchTerm = $('#foodChoice').val()
+    restaurantsAdapter(searchTerm).done(function(){
+
+      // something 
+
+
     })
-
-    // adapter
-
-
-
   })
+
 })
-
-
-
-  // And place the new image tag in the appropriate html.
-//   $.ajax({
-//   method: "POST",
-//   url: "some.php",
-//   data: { name: "John", location: "Boston" }
-// })
-//   .done(function( msg ) {
-//     alert( "Data Saved: " + msg );
-//   });
--- auth.js
+++ auth.js
@@ -1,30 +1,47 @@
+var authURL = 'https://api.yelp.com/oauth2/token'
+
+function auth(authParams){
+  return $.ajax({
+    method: "POST",
+    contentType: 'application/x-www-form-urlencoded',
+    dataType: 'jsonp',
+    crossDomain: true,
+    url: `${authURL}?grant_type=${authParams.grant_type}&client_id=${authParams.app_id}&client_secret=${authParams.app_secret}`
+  }).done(function( response ) {
+    console.log(response)
+  })
+}
+
+
 // https://www.yelp.com/developers/documentation/v2/authentication
 // https://www.npmjs.com/package/oauth-signature
 
-var currentTime = new Date($.now());
-function generateNonce() {
-  // A unique string randomly generated per request.
-    var nonce = '';
-    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    for (var i = 0; i < 16; i++) nonce += chars[Math.floor(Math.random() * chars.length)];
-    return nonce;
-}
+// var currentTime = new Date($.now());
 
-var httpMethod = 'GET',
-var yelpUrl = 'https://api.yelp.com/v2/search?term=food&location=San+Francisco',
+// function generateNonce() {
+//   // A unique string randomly generated per request.
+//     var nonce = '';
+//     var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     for (var i = 0; i < 16; i++) nonce += chars[Math.floor(Math.random() * chars.length)];
+//     return nonce;
+// }
 
-var parameters = {
-        oauth_consumer_key : yelpAuth.consumerKey,
-        oauth_token : yelpAuth.accessToken,
-        oauth_nonce : generateNonce(),
-        oauth_timestamp : currentTime,
-        oauth_signature_method : yelpAuth.serviceProvider.signatureMethod,
-        oauth_version : '1.0'
-    },
-var consumerSecret = yelpAuth.consumerSecret,
-var tokenSecret = yelpAuth.accessTokenSecret,
-    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash 
-var encodedSignature = oauthSignature.generate(httpMethod, yelpUrl, parameters, consumerSecret, tokenSecret),
-    // generates a BASE64 encode HMAC-SHA1 hash 
-var signature = oauthSignature.generate(httpMethod, yelpUrl, parameters, consumerSecret, tokenSecret,
-{ encodeSignature: false});
+// var httpMethod = 'GET'
+// var yelpBaseUrl = 'https://api.yelp.com/v2/search'
+
+// var parameters = {
+//         oauth_consumer_key : yelpAuth.consumerKey,
+//         oauth_token : yelpAuth.accessToken,
+//         oauth_nonce : generateNonce(),
+//         oauth_timestamp : currentTime,
+//         oauth_signature_method : yelpAuth.serviceProvider.signatureMethod,
+//         oauth_version : '1.0'
+//     }
+// var consumerSecret = yelpAuth.consumerSecret
+// var tokenSecret = yelpAuth.accessTokenSecret
+
+
+//     // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash 
+// var encodedSignature = oauthSignature.generate(httpMethod, yelpBaseUrl, parameters, consumerSecret, tokenSecret)
+// //     // generates a BASE64 encode HMAC-SHA1 hash 
+// var signature = oauthSignature.generate(httpMethod, yelpBaseUrl, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
