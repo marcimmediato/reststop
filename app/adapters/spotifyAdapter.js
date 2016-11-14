9529b8cf98632264ca10cdbdbe56c7b234733234
wcpines <colby@aptible.com>
2016-11-14 14:21:31 -0500

    Fix html display

--- app/adapters/spotifyAdapter.js
+++ app/adapters/spotifyAdapter.js
@@ -8,12 +8,12 @@ function spotifyAdapter(searchTerm){
   }).done(function(returnObject) {
     let name = returnObject.tracks.items[0].name
     let link = returnObject.tracks.items[0].external_urls.spotify
-    let artist = returnObject.tracks.album.artists[0].name
+    let artist = returnObject.tracks.items[0].album.artists[0].name
     addSong(name, link, artist)
   })
 }
 
 function addSong(name, link, artist){
-  html=`<b>Listen a tasty song by ${artist}:</b> <br> <em><a href=${link}><${name}</a></em>`
+  html=`<b>Listen a tasty song by ${artist}:</b><br><em><a href='${link}' target='_blank'>${name}</a></em>`
   $('#songRequest').empty().append(html)
 }
