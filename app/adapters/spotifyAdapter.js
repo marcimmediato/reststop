2394f159a3b7e911dc23d442cc1584ad2abad182
wcpines <colby@aptible.com>
2016-11-14 18:38:38 -0500

    Things are cooler now

--- app/adapters/spotifyAdapter.js
+++ app/adapters/spotifyAdapter.js
@@ -6,14 +6,20 @@ function spotifyAdapter(searchTerm){
     dataType: 'JSON',
     crossDomain: true,
   }).done(function(returnObject) {
-    let name = returnObject.tracks.items[0].name
-    let link = returnObject.tracks.items[0].external_urls.spotify
-    let artist = returnObject.tracks.items[0].album.artists[0].name
-    addSong(name, link, artist)
+
+    let rand = Math.floor(Math.random() * 20);
+    let name = returnObject.tracks.items[rand].name
+    let link = returnObject.tracks.items[rand].external_urls.spotify
+    let uri = returnObject.tracks.items[rand].uri
+    let artist = returnObject.tracks.items[rand].album.artists[0].name
+    addSong(name, link, uri, artist)
   })
 }
 
-function addSong(name, link, artist){
-  html=`<b>Listen a tasty song by ${artist}:</b><br><em><a href='${link}' target='_blank'>${name}</a></em>`
-  $('#songContent').append(html)
+function addSong(name, link, uri, artist){
+  html = `<b>Listen a tasty song by ${artist}:</b><br><em><a href='${link}' target='_blank'>${name}</a></em>`
+  player = `<iframe src="https://embed.spotify.com/?uri=${uri}" frameborder="0" allowtransparency="true"></iframe>`
+  $('#songContent').empty().append(html).append(player)
 }
+
+
