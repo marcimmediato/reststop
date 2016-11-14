4bb6f7ea7430c79691eecba3b16bb592c1e98ab0
marcimmediato <marc.immediato@gmail.com>
2016-11-14 16:48:44 -0500

    fix pizza population

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -2,13 +2,13 @@ const Restaurant = (function() {
 
   return class Restaurant {
 
-    constructor(name, address, latitude, longitude, checkins, stopId){
+    constructor(name, address, latitude, longitude, checkins, stopNum){
       this.name = name;
       this.address = address;
       this.latitude = latitude;
       this.longitude = longitude;
       this.checkins = checkins;
-      this.stopId = stopId;
+      this.stopNum = stopNum;
       store.restaurants.push(this);
     }
   }
